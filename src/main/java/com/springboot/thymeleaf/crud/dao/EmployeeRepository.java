c1c83f66f832c98bc50d63ba91c79be210fc28d0
karthikeyan <karthirm90@gmail.com>
2021-10-28 20:49:55 +0800

    jpa repository class change

--- src/main/java/com/springboot/thymeleaf/crud/dao/EmployeeRepository.java
+++ src/main/java/com/springboot/thymeleaf/crud/dao/EmployeeRepository.java
@@ -5,10 +5,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.springboot.thymeleaf.crud.entity.Employee;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-	// that's it ... no need to write any code LOL!
 	
 	// add a method to sort by last name
 	public List<Employee> findAllByOrderByLastNameAsc();
